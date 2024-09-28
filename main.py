42e4841f34da6e8a75c3e23e99110b17b51a16d9
muhonja <linahmuhonjaimbari@gmail.com>
2024-09-28 08:48:18 +0300

    add error handling for FileExists error

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile, Form
 from datetime import date
-from sqlalchemy.orm  import Session
+from sqlalchemy.orm import Session
 from secrets import token_hex
+import os
 
-from  sql_app import models, schemas, crud
-from  sql_app.database import SessionLocal, engine
+from sql_app import models, schemas, crud
+from sql_app.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
 
 # Dependency
 def get_db():
@@ -19,34 +21,36 @@ def get_db():
 
 
 @app.post('/users/', response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail='Email already exists')
     return crud.create_user(db=db, user=user)
 
+
 @app.get('/users/{user_id}', response_model=schemas.User)
-def read_user(user_id: int, db: Session=Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return db_user
 
+
 @app.post('/contracts', response_model=schemas.Contract)
 async def create_contract(
-        contract_name: str=Form(...),
-        category: str=Form(...),
-        start_date: date=Form(...),
-        end_date: date=Form(...),
-        country: schemas.Country=Form(...),
-        vendor_name: str=Form(...),
-        company_name: schemas.Company=Form(...),
-        expired_status: bool=Form(...),
-        file: UploadFile=File(...),
-        db: Session=Depends(get_db)):
+        contract_name: str = Form(...),
+        category: str = Form(...),
+        start_date: date = Form(...),
+        end_date: date = Form(...),
+        country: schemas.Country = Form(...),
+        vendor_name: str = Form(...),
+        company_name: schemas.Company = Form(...),
+        expired_status: bool = Form(...),
+        file: UploadFile = File(...),
+        db: Session = Depends(get_db)):
     required_ext = {'pdf', 'doc', 'docx'}
     print(f"{contract_name}")
-    db_contract = crud.get_contract_by_name(db=db, contract_name = contract_name)
+    db_contract = crud.get_contract_by_name(db=db, contract_name=contract_name)
     if db_contract:
         print(f"the contract exists {db_contract}")
         raise HTTPException(status_code=400, detail='Contract already exists')
@@ -78,33 +82,37 @@ async def create_contract(
             country=country,
             vendor_name=vendor_name,
             company_name=company_name,
-            expired_status=expired_status,
+            status=expired_status,
             file_upload=file_path
         )
         return crud.create_contract(db, contract=contract_data)
 
+
 @app.get('/contracts/{contract_id}', response_model=schemas.Contract)
-def read_contract(contract_id: int, db: Session=Depends(get_db)):
+def read_contract(contract_id: int, db: Session = Depends(get_db)):
     db_contract = crud.get_contract(db, contract_id)
     if db_contract is None:
         raise HTTPException(status_code=40, detail='Contract not found')
     return db_contract
 
+
 @app.get('/contracts/', response_model=list[schemas.Contract])
-def get_contracts(db: Session=Depends(get_db), skip: int=0, limit: int=100):
+def get_contracts(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     db_contracts = crud.get_contracts(db, skip=skip, limit=limit)
     return db_contracts
 
+
 @app.delete('/contracts/{contract_id}')
-def delete_contract(contract_id: int, db: Session=Depends(get_db)):
+def delete_contract(contract_id: int, db: Session = Depends(get_db)):
     db_contract = crud.get_contract(db, contract_id)
     print(db_contract)
     if db_contract is None:
         raise HTTPException(status_code=404, detail='Contract not found')
     return crud.delete_contract(db, contract_id)
 
+
 @app.put('/contracts/{contract_id}', response_model=schemas.Contract)
-def update_contract(contract_id: int, contract: schemas.Contract, db: Session=Depends(get_db)):
+def update_contract(contract_id: int, contract: schemas.Contract, db: Session = Depends(get_db)):
     db_contract = crud.get_contract(db, contract_id)
     if db_contract is None:
         raise HTTPException(status_code=404, detail='Contract not found')
