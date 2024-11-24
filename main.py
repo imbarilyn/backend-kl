3476cf07bc4f54b1f9f874827a9c5691f58b625c
muhonja <linahmuhonjaimbari@gmail.com>
2024-11-24 10:46:32 +0300

    add  endpoint for adding email addresses

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ def get_db():
         db.close()
 
 
+
+
 @app.post('/users/', response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -57,16 +59,16 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post('/contracts', response_model=schemas.Contract)
+@app.post('/add-contracts')
 async def create_contract(
         contract_name: str = Form(...),
         category: str = Form(...),
-        start_date: date = Form(...),
-        end_date: date = Form(...),
+        start_date: str = Form(...),
+        end_date: str = Form(...),
         country: schemas.Country = Form(...),
         vendor_name: str = Form(...),
         company_name: schemas.Company = Form(...),
-        expired_status: bool = Form(...),
+        status: str = Form(...),
         file: UploadFile = File(...),
         db: Session = Depends(get_db)):
     required_ext = {'pdf', 'doc', 'docx'}
@@ -74,7 +76,10 @@ async def create_contract(
     db_contract = crud.get_contract_by_name(db=db, contract_name=contract_name)
     if db_contract:
         print(f"the contract exists {db_contract}")
-        raise HTTPException(status_code=400, detail='Contract already exists')
+        return {'message': 'Contract already exists', 'result': 'fail'}
+        # raise HTTPException(status_code=400, detail='Contract already exists')
+
+
     if db_contract is None:
         print(f"The contract does not exist yet {db_contract}")
         file_ext = file.filename.split('.').pop().lower()
@@ -86,7 +91,7 @@ async def create_contract(
         # Ensure the uploads directory exists
         try:
             os.makedirs('uploads', exist_ok=True)
-            file_path = os.path.join('uploads', f"{file_name}. {file_ext}")
+            file_path = os.path.join('uploads', f"{file_name}.{file_ext}")
              # the  f is an object created once the file is open
             # we use 'wb' format tto write binary data to file for it may content images or pdfs
             with open(file_path, 'wb') as f:
@@ -103,27 +108,31 @@ async def create_contract(
             country=country,
             vendor_name=vendor_name,
             company_name=company_name,
-            status=expired_status,
+            status=status,
             file_upload=file_path
         )
         return crud.create_contract(db, contract=contract_data)
 
 
-@app.get('/contracts/{contract_id}', response_model=schemas.Contract)
+@app.get('/contract/{contract_id}')
 def read_contract(contract_id: int, db: Session = Depends(get_db)):
     db_contract = crud.get_contract(db, contract_id)
     if db_contract is None:
-        raise HTTPException(status_code=40, detail='Contract not found')
-    return db_contract
+        return {'message': 'Contract not found', 'result': 'fail'}
+        # raise HTTPException(status_code=40, detail='Contract not found')
+
+    return {'message': 'Contract found', 'result': 'success', 'data': db_contract}
 
 
-@app.get('/contracts/', response_model=list[schemas.Contract])
-def get_contracts(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
-    db_contracts = crud.get_contracts(db, skip=skip, limit=limit)
+@app.get('/contracts/')
+def get_contracts(db: Session = Depends(get_db)):
+    db_contracts = crud.get_contracts(db)
+    # db_contracts = db.query(models.Contract).all()
+    print(db_contracts)
     return db_contracts
 
 
-@app.delete('/contracts/{contract_id}')
+@app.delete('/delete-contract/{contract_id}')
 def delete_contract(contract_id: int, db: Session = Depends(get_db)):
     db_contract = crud.get_contract(db, contract_id)
     print(db_contract)
@@ -132,9 +141,27 @@ def delete_contract(contract_id: int, db: Session = Depends(get_db)):
     return crud.delete_contract(db, contract_id)
 
 
-@app.put('/contracts/{contract_id}', response_model=schemas.Contract)
+@app.put('/update-contract/{contract_id}')
 def update_contract(contract_id: int, contract: schemas.Contract, db: Session = Depends(get_db)):
+    print(f"the contract is {contract}")
     db_contract = crud.get_contract(db, contract_id)
     if db_contract is None:
-        raise HTTPException(status_code=404, detail='Contract not found')
+        # raise HTTPException(status_code=404, detail='Contract not found')
+        return{'message': 'Contract not found', 'result': 'fail'}
     return crud.update_contract(db, contract)
+
+
+@app.post('/add-emails')
+def add_email(email: sql_app.schemas.ExpiryEmailBase, db: Session = Depends(get_db)):
+    print(f"Email: {email}")
+
+    db_email = crud.get_email_by_name(db, email)
+    if db_email:
+        return {'message': 'Email already exists', 'result': 'fail'}
+    return crud.add_email(db, email)
+
+@app.get('/emails/')
+def get_emails(db: Session = Depends(get_db)):
+    emails = crud.get_emails(db)
+    print(emails)
+    return emails
