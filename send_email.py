b63115caaa37c7c61c324a82aa08113855b53bca
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-18 16:42:13 +0300

    add function to send emails

--- send_email.py
+++ send_email.py
@@ -4,7 +4,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 from email.message import EmailMessage
 from email.utils import formataddr
-
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from sqlalchemy import and_
+from sql_app import models, schemas
+from main import get_db
+from fastapi import FastAPI
 # Email server details
 
 
@@ -13,7 +19,7 @@ SMTP_SERVER = 'smtp.gmail.com'
 
 # Trying to get the current directory to load the environment variables
 # The script below works for both file-based or interactive python sessions like REPL
-current_dir = Path(__file__).resolve().parent if '__file__' in locals() else Path.cwd()
+current_dir = Path(__file__).resolve().parent if '__fi*.pyle__' in locals() else Path.cwd()
 env_directory = current_dir / '.env'
 
 # Now let's load the environment variables to get the email credentials
@@ -74,11 +80,49 @@ def send_email_expired_contract(subject, receiver_email, expiry_date, contract_n
         print('Email sent successfully')
 
 
-if __name__ == '__main__':
-    send_email_expired_contract(
-        'Contract Expiry Notification',
-        receiver,
-        "2021-09-30",
-        'Rodl Advocates',
-        'Rodl Advocates',
-    )
+
+def get_emails(db: Session):
+    emails = db.query(models.ExpiryEmail).all()
+    print(emails)
+    if emails:
+        email_list = [email for email in emails]
+        return email_list
+
+
+def contracts_mail_not_sent_expiry_three_months():
+    print('We are running a cron job')
+    today = datetime.now()
+    threshold_date = today + timedelta(days=90)
+    db: Session= next(get_db())
+    contracts = db.query(models.Contract).filter(
+        and_(
+            models.Contract.email_sent == 0,
+            # datetime.strptime(models.Contract.end_date, '%Y-%m-%d') <= threshold_date
+            models.Contract.end_date <= threshold_date.strftime('%Y-%m-%d')
+        )
+       ).all()
+    emails = get_emails(db)
+    if contracts:
+        for contract in contracts:
+            for email in emails:
+                send_email_expired_contract(
+                    'Contract Expiry Notification',
+                    email.email,
+                    contract.end_date,
+                    contract.contract_name,
+                    contract.vendor_name
+                )
+                contract.email_sent = 1
+                db.commit()
+    print('No contracts to send mails to')
+
+
+
+contracts_mail_not_sent_expiry_three_months()
+# get_emails()
+#
+# if __name__ == '__main__':
+#     contracts_mail_not_sent_expiry_three_months()
+#     get_emails()
+#     print('We are running a cron job')
+#     print('Emails sent successfully')
