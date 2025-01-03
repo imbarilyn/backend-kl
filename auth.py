460cb377269c02a838f509cfbc77aada4d226b1e
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-03 23:54:55 +0300

    load secret key from .env file

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+import os
 from os import close
 
 from passlib.context import CryptContext
@@ -5,6 +6,7 @@ import jwt
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing_extensions import deprecated
 
+from main import current_dir, env_directory, SECRET_KEY
 from sql_app import schemas, models
 from jwt.exceptions import InvalidTokenError
 from datetime import timedelta, datetime
@@ -12,6 +14,7 @@ from fastapi import Depends, status, HTTPException,APIRouter
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from sql_app.database import  SessionLocal, engine
+from pathlib import Path
 
 
 router = APIRouter(
@@ -19,7 +22,12 @@ router = APIRouter(
     tags=['auth']
 )
 
-
+current_dir = Path(__file__).resolve().parent if __file__ in locals() else Path.cwd()
+env_directory = current_dir  / '.env'
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITH='HS256'
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 
