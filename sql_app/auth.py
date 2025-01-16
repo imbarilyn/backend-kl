89b390c302371194f6e4b9208291dcfae0d996fc
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-16 19:08:51 +0300

    add get_user_by_email function

--- sql_app/auth.py
+++ sql_app/auth.py
@@ -93,6 +93,9 @@ def verify_password(plain_password: str, hashed_password: str):
 def get_user(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user(db, username)
     print(f"user hashed password {user.hashed_password}")
