11ba3328a6af8429ad229e8e5ff84b5b043313cc
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-05-30 07:03:43 +0300

    load frontend_url from .env file

--- reset_password_mail.py
+++ reset_password_mail.py
@@ -6,19 +6,23 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import BackgroundTasks
 
-from notification_mail import SMTP_SERVER, password_email
+from app.routes.reset_pasword_email import frontend_url
 
 PORT = 587
-SMTP_SERVER = 'smtp.gmail.com'
+
 
 current_directory = Path(__file__).resolve().parent if '__file__' in locals()else Path.cwd()
 env_directory = current_directory / '.env'
 
 # load env variables
-load_dotenv(env_directory)
+# load_dotenv(env_directory)
+load_dotenv("/backend-kl/.env")
 
 sender_email = os.getenv('SENDER_EMAIL')
 password_email = os.getenv('EMAIL_PASSWORD')
+smtp_server = os.getenv('SMTP_SERVER')
+frontend_url = os.getenv('FRONTEND_URL')
+
 
 
 def send_reset_password_mail(background_tasks: BackgroundTasks, receiver_mail, username, token):
@@ -33,14 +37,14 @@ def send_reset_password_mail(background_tasks: BackgroundTasks, receiver_mail, u
                 <p>Hello,<p>
                 <span>{username}</span>
                 <p>Please click below to reset your password</p>
-                <a href="http://localhost:5173/auth/reset-password?token={token}">Reset Password</a>
+                <a href=f"{frontend_url}/auth/reset-password?token={token}">Reset Password</a>
             </body>
         </htm/>
 """,
         subtype='html'
     )
 
-    with smtplib.SMTP(SMTP_SERVER, PORT)as server:
+    with smtplib.SMTP(smtp_server, PORT)as server:
         server.starttls()
         server.login(sender_email, password_email)
         server.sendmail(sender_email, receiver_mail, msg.as_string())
